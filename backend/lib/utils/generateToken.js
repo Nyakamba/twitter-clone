dcbb8f87bfdcd2ea1d8b2a949b3f86719127950a
Nyakamba <omwegaenock@gmail.com>
2024-07-03 22:43:33 +0300

    compl;eted auth controllers

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -6,7 +6,7 @@ export const generateTokenAndSetCookie = async (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 15 * 24 * 60860 * 1000,
+    maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true,
     sameSite: "strict",
     secure: process.env.NODE_ENV !== "development",
