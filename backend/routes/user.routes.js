c97a40989b26e580d3edc7de31bb4a154170921e
Nyakamba <omwegaenock@gmail.com>
2024-07-04 22:20:04 +0300

    complete the follow unfollow functionality

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,9 @@
 import express from "express";
 import { protectRoute } from "../middlewares/protectRoute.js";
-import { getUserProfile } from "../controllers/user.controller.js";
+import {
+  followUnfollowUser,
+  getUserProfile,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
