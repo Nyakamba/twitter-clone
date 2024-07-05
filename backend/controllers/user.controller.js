d463f6d77425349847fe763ae73689af05c6694f
Nyakamba <omwegaenock@gmail.com>
2024-07-05 21:11:50 +0300

    completed the notification api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Notification from "../models/notification.model.js";
 import User from "../models/user.model.js";
 
 export const getUserProfile = async (req, res) => {
@@ -47,8 +48,14 @@ export const followUnfollowUser = async (req, res) => {
       await User.findByIdAndUpdate(req.user._id, {
         $push: { following: id },
       });
-      //send notification to the user
+      //send notification to user
+      const newNotification = new Notification({
+        type: "follow",
+        from: req.user._id,
+        to: userToModify._id,
+      });
 
+      await newNotification.save();
       res.status(200).json({ message: "User followed sunccessfully" });
     }
   } catch (error) {
