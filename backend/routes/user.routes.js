5029b6cfe227ff6f42b9a4de6503847c49b5b30e
Nyakamba <omwegaenock@gmail.com>
2024-07-06 22:03:40 +0300

    user notification implementation

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -8,7 +8,7 @@ import {
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
-// router.get("/suggested", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
 // router.post("/update", protectRoute, updateUserProfile);
 
