944e4d34ea86315681ac3224f5033afc3b634a1e
Nyakamba <omwegaenock@gmail.com>
2024-07-07 21:18:12 +0300

    get suggested users

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,6 +3,8 @@ import { protectRoute } from "../middlewares/protectRoute.js";
 import {
   followUnfollowUser,
   getUserProfile,
+  getSuggestedUsers,
+  updateUser,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
@@ -10,6 +12,6 @@ const router = express.Router();
 router.get("/profile/:username", protectRoute, getUserProfile);
 router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
-// router.post("/update", protectRoute, updateUserProfile);
+router.post("/update", protectRoute, updateUser);
 
 export default router;
