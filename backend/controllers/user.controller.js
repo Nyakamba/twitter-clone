a80ebca376b4438c06eeb37c1a8cc79b46d8b563
Nyakamba <omwegaenock@gmail.com>
2024-07-07 21:56:14 +0300

    user  routes and controllers

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import Notification from "../models/notification.model.js";
 import User from "../models/user.model.js";
+import { v2 as cloudinary } from "cloudinary";
 
 export const getUserProfile = async (req, res) => {
   const { username } = req.params;
