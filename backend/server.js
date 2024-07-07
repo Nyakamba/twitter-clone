064bb005f95970ee9ba550f9a8d91f25bfa4de46
Nyakamba <omwegaenock@gmail.com>
2024-07-07 22:07:02 +0300

    post routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
