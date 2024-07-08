24f0cfaaf6a7567d79d9e783a32c213b87173090
Nyakamba <omwegaenock@gmail.com>
2024-07-08 20:17:21 +0300

    post routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is runnging on port ${PORT}`);
