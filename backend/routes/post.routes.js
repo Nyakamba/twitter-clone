dd2fce89a31d401993eee8ea22095ad447db61ed
Nyakamba <omwegaenock@gmail.com>
2024-07-08 21:24:20 +0300

    post routes and controllers

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { protectRoute } from "../middlewares/protectRoute.js";
 import {
-  //   commentOnPost,
+  commentOnPost,
   createPost,
-  //   deletePost,
+  deletePost,
   //getAllPosts,
   //   getFollowingPosts,
   //   getLikedPosts,
   //   getUserPosts,
-  //   likeUnlikePost,
+  likeUnlikePost,
 } from "../controllers/post.controllers.js";
 
 const router = express.Router();
@@ -18,8 +18,8 @@ const router = express.Router();
 // router.get("/likes/:id", protectRoute, getLikedPosts);
 // router.get("/user/:username", protectRoute, getUserPosts);
 router.post("/create", protectRoute, createPost);
-// router.post("/like/:id", protectRoute, likeUnlikePost);
-// router.post("/comment/:id", protectRoute, commentOnPost);
-// router.delete("/:id", protectRoute, deletePost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+router.delete("/:id", protectRoute, deletePost);
 
 export default router;
