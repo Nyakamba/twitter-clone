633cd09ee797950e4637f118b5f04b5940f8ec32
Nyakamba <omwegaenock@gmail.com>
2024-07-26 22:06:15 +0300

    login page completed

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -18,7 +18,7 @@ const LoginPage = () => {
   const queryClient = useQueryClient();
   const { mutate, isError, isPending, error } = useMutation({
     onSuccess: () => {
-      toast.success("Logged in created successfully");
+      toast.success("Logged in  successfully");
 
       queryClient.invalidateQueries({ queryKey: ["authUser"] });
     },
