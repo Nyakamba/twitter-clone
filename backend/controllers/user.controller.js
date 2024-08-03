3c3b61700302b1bef47e192ef8affa8b69f1dd57
Nyakamba <omwegaenock@gmail.com>
2024-08-03 15:27:44 +0300

    comment deleted

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -42,7 +42,7 @@ export const followUnfollowUser = async (req, res) => {
       await User.findByIdAndUpdate(req.user._id, {
         $pull: { following: id },
       });
-      //return the id of the user as a response
+
       res.status(200).json({ message: "User unfollowed sunccessfully" });
     } else {
       //follow the user
